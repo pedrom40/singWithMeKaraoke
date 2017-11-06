cdf927f986956dcfce965fa268b64c773401d54c
pedrom40 <pedro.morin1980@gmail.com>
2017-11-06 10:16:27 -0600

    added necessary npm packages

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+// include libraries
 const express = require('express');
-
 const app = express();
 
+// setup middleware
 app.use(express.static('public'));
+
 
 // root domain
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
 
 // setup server
 app.listen(process.env.PORT || 8080, () => {
