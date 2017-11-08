2898a4183dc2341f3805689b496def2a7291e42b
pedrom40 <pedro.morin1980@gmail.com>
2017-11-08 06:14:38 -0600

    setup mongo db, setup server for exports, include all packages needed for protected endpoints

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 // include libraries
+require('dotenv').config();
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
+// app setup
+mongoose.Promise = global.Promise;
+const {PORT, DATABASE_URL} = require('./config');
 const app = express();
 
+// Logging
+app.use(morgan('common'));
+
+// CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+app.use(passport.initialize());
+//passport.use(basicStrategy);
+//passport.use(jwtStrategy);
 
 // setup middleware
 app.use(express.static('public'));
@@ -12,8 +38,50 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// catch all method
+app.use('*', (req, res) => {
+  return res.status(404).json({message: 'Not Found'});
+});
+
 
 // setup server
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+let server;
+
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+      .listen(PORT, () => {
+        console.log(`Your app is listening on port ${PORT}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
